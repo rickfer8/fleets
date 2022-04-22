126b36d7b1fba1f22367c0727c0cca5f2c3e889a
Thiago <thiagoferreiradeaguiar@gmail.com>
2022-04-21 22:33:11 -0300

    ordenacao usuários e perfis
    ordenacao usuários e perfis

--- fontes/backend-fleets/src/main/java/com/fleets/seguros/service/PerfilService.java
+++ fontes/backend-fleets/src/main/java/com/fleets/seguros/service/PerfilService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,7 +27,7 @@ public class PerfilService {
 	private PerfilDAOImpl perfilDAOImpl;
 
 	public List<Perfil> findAll() {
-		return perfilDAO.findAll();
+		return perfilDAO.findAll(Sort.by(Sort.Direction.ASC, "descricao"));
 	}
 
 	public Perfil getById(Long id) {
--- fontes/backend-fleets/src/main/java/com/fleets/seguros/service/UsuarioService.java
+++ fontes/backend-fleets/src/main/java/com/fleets/seguros/service/UsuarioService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,7 +29,7 @@ public class UsuarioService {
 	private UsuarioDAOImpl usuarioDAOImpl;
 
 	public List<Usuario> findAll() {
-		return usuarioDAO.findAll();
+		return usuarioDAO.findAll(Sort.by(Sort.Direction.ASC, "nome"));
 	}
 
 	public Usuario getById(Long id) {
