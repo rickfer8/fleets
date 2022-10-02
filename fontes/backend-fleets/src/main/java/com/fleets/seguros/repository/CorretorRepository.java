d2f9628eb436ae9ad8386201061cd1826b33f6e8
Thiago <thiagoferreiradeaguiar@gmail.com>
2022-10-02 11:30:03 -0300

    exclusão de corretor

--- fontes/backend-fleets/src/main/java/com/fleets/seguros/repository/CorretorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fleets.seguros.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fleets.seguros.model.Corretor;
-
-public interface CorretorRepository extends JpaRepository<Corretor, Long>{
-
-}
